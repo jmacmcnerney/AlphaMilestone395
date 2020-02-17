733343df755b063b89ee89c0f3aeaf930e15a3a3
John Colfer <jpcolfer99@gmail.com>
2020-02-17 10:50:14 -0500

    Removed some useless comments

--- functions.cpp
+++ functions.cpp
@@ -28,26 +28,19 @@ void Action(string command, bool waitUntilSuccess) {
 
 //Character Creation Function
 void SetupCharacter(string name, string bodyType, string clothing, string hairStyle, string hairColor, string position) {
-	//string actionString = "";
-
-	//actionString = "CreateCharacter(" + name + ", " + bodyType + ")";
+	
 	Action("CreateCharacter(" + name + ", " + bodyType + ")", true);
 
 	if (clothing != "") {
-		//actionString = "SetClothing(" + name + ", " + clothing + ")";
-		//Action(actionString, true);
 		Action("SetClothing(" + name + ", " + clothing + ")", true);
 	}
 	if (hairStyle != "") {
-		//actionString = "SetHairStyle(" + name + ", " + hairStyle + ")";
 		Action("SetHairStyle(" + name + ", " + hairStyle + ")", true);
 	}
 	if (hairColor != "") {
-		//actionString = "SetHairColor(" + name + ", " + hairColor + ")";
 		Action("SetHairColor(" + name + ", " + hairColor + ")", true);
 	}
 	if (position != "") {
-		//actionString = "SetPosition(" + name + ", " + position + ")";
 		Action("SetPosition(" + name + ", " + position + ")", true);
 	}
 }
@@ -57,40 +50,33 @@ void SetupIcons(vector<string> titles, vector<string> icons, vector<string> obje
 	
 	int vectorSize = titles.size();
 	int sentinal = 0;
-	//string actionString = "";
 
 	while (sentinal < vectorSize) {
-		//actionString = "EnableIcon(" + titles[sentinal] + ", " + icons[sentinal] + ", " + objects[sentinal] + ", " + descriptions[sentinal] + ", " + defaults[sentinal] + ")";
 		Action("EnableIcon(" + titles[sentinal] + ", " + icons[sentinal] + ", " + objects[sentinal] + ", " + descriptions[sentinal] + ", " + defaults[sentinal] + ")", true);
 		sentinal++;
 	}
 
-	//Action("EnableIcon(Open_Door, Open, BobsHouse.Door, Leave the house, true)", true);
 }
 
+//Does the inital setup for Dialog
 void SetupDialog(string mainChar, string talkingTo) {
 	string actionString = "";
 
 	Action("DisableInput()", true);
 
-	//actionString = "WalkTo(" + mainChar + ", " + talkingTo + ")";
 	Action("WalkTo(" + mainChar + ", " + talkingTo + ")", true);
 	Action("ShowDialog()", true);
 	Action("ClearDialog()", true);
 
-	//actionString = "SetLeft(" + mainChar + ")";
 	Action("SetLeft(" + mainChar + ")", true);
-	//Action("SetLeft(Bob)", true);
 
-	//actionString = "SetRight(" + mainChar + ")";
 	Action("SetRight(" + talkingTo + ")", true);
-	//Action("SetRight(Grandmother)", true);
 	Action("EnableInput()", true);
 }
 
+//Handles all of purchasing something for a coin
 void PurchaseFunct(string purchase, string input, vector<string> & storeInv, vector<string> & inventory, bool & coin, bool & purchaseCheck) {
 	
-//	else if (i == "input Buy_Sword " + purchase) {
 	Action("RemoveFromList(" + purchase + ")", true);
 	for (int i = 0; i < storeInv.size(); i++) {
 		if (storeInv[i] == purchase) {
